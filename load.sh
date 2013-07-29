7c33548c3b804265a9d1b660b6877691cbb0493b
Craig Cook <boycook@me.com>
2013-07-29 16:47:34 +0100

    Updated load.sh for file renames

--- load.sh
+++ load.sh
@@ -2,8 +2,8 @@
 
 
 tsapp push spaceui index.html
-tsapp push spaceui dom.js
 tsapp push spaceui html.js
+tsapp push spaceui htmlgenerator.js
 tsapp push spaceui spaceui.js
 tsapp push spaceui spaceui.css
 tsapp push spaceui uilayout.css
