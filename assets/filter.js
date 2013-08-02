2240db4757beae5f5f4a79dfd661a3e35ae6b8d5
Craig Cook <boycook@me.com>
2013-08-02 14:56:33 +0100

    Made filter case insensitive

--- assets/filter.js
+++ assets/filter.js
@@ -5,10 +5,11 @@ function Filter(data, operation) {
 }
 
 Filter.prototype.filter = function(key, value) {
+	value = value.toLowerCase();
 	var filtered = [];
 	for (var i=0,len=this.data.length; i<len; i++) {
 		var item = this.data[i];
-		if (item.hasOwnProperty(key) && item[key].indexOf(value) > -1) {
+		if (item.hasOwnProperty(key) && item[key].toLowerCase().indexOf(value) > -1) {
 			filtered.push(item);
 		}
 	}
