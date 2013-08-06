fb34986e9a0444945e78a80d61488f5e9a93e1cf
Craig Cook <boycook@me.com>
2013-08-06 14:01:06 +0100

    Bringing back explicit loaf

--- load.sh
+++ load.sh
@@ -14,4 +14,23 @@ function load_dir() {
 	cd ../
 }
 
-load_dir assets
+function load_explicit() {
+	tsapp push spaceui Elusive-Icons.eot
+	tsapp push spaceui Elusive-Icons.svg
+	tsapp push spaceui Elusive-Icons.ttf
+	tsapp push spaceui Elusive-Icons.woff
+	tsapp push spaceui elusive-webfont.css
+	tsapp push spaceui jquery.growl.css
+	tsapp push spaceui index.html
+	tsapp push spaceui jquery.growl.js
+	tsapp push spaceui html.js
+	tsapp push spaceui htmlgenerator.js
+	tsapp push spaceui http.js
+	tsapp push spaceui filter.js
+	tsapp push spaceui space.js
+	tsapp push spaceui spaceui.js
+	tsapp push spaceui spaceui.css
+	tsapp push spaceui uilayout.css	
+}
+
+# load_dir assets
