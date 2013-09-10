ba258b067dda7100bbe2522f5c22120e8081c93f
Craig Cook <boycook@me.com>
2013-09-10 16:09:31 +0100

    Testing sort descending

--- test/spec/SortSpec.js
+++ test/spec/SortSpec.js
@@ -16,13 +16,21 @@ describe('Sort', function () {
     });
 
     describe('#sort', function () {
-        it('should sort ok ', function () {
+        it('should ascending sort ok ', function () {
             var sorted = new Sort(data).sort('key');
             sorted[0].should.eql({ key: 'a' });
             sorted[1].should.eql({ key: 'b' });
             sorted[2].should.eql({ key: 'c' });
             sorted[3].should.eql({ key: 'd' });
             sorted[4].should.eql({ key: 'e' });
+        });
+        it('should descending sort ok ', function () {
+            var sorted = new Sort(data).sort('-key');
+            sorted[0].should.eql({ key: 'e' });
+            sorted[1].should.eql({ key: 'd' });
+            sorted[2].should.eql({ key: 'c' });
+            sorted[3].should.eql({ key: 'b' });
+            sorted[4].should.eql({ key: 'a' });
         });
     });
 });
