9192123e84670156fcab729f0d8e888b435d3a51
Craig Cook <boycook@me.com>
2013-10-04 11:12:10 +0100

    Added test for Http

---
+++ test/HttpSpec.js
@@ -0,0 +1,17 @@
+var should = require('should');
+var fs = require('fs');
+var vm = require('vm');
+var HTTP = require('../assets/http.js').HTTP;
+
+describe('HTTP', function () {
+	
+    before(function() {
+    	// vm.runInThisContext(fs.readFileSync('assets/'));
+    });
+
+    describe('#HTTP', function () {
+        it('should work', function () {
+
+        });
+    });
+});
